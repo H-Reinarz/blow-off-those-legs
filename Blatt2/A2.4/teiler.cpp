887d5bbfcbbb79d0d667f06a8c7ef84cddeb0885
Omnishambles <Guentheg@students.uni-marburg.de>
2016-10-27 21:28:46 +0200

    Loesungsvorschlag fuer 2.4 mit Primfaktorzerlegung

--- Blatt2/A2.4/teiler.cpp
+++ Blatt2/A2.4/teiler.cpp
@@ -33,7 +33,34 @@ int main (void){
 	printf("\n");
 	
 	// Part c)
+	int nPrime = n;
 	
+	printf("Primfaktorzerlegung: ");
+	// Count through all Numbers smaller than n
+	for (int i = 2; i <= n; i++){
+		
+		// Check if prime number like in part b)
+		for (int j = 1; j < i; j++){
+			
+			// if number i is no prime number -> break!
+			if (j > 1 && i % j == 0){
+				break;
+			}
+			
+			// if number i is a prime number
+			if (j == i-1) {
+				// while nPrime is divisible by prime number(i) without remainder -> divide!
+				while (nPrime % i == 0){
+					nPrime = nPrime / i;
+					printf("%d  ",i);
+				}
+			}
+		}
+		// If nPrime == 1 -> break!
+		if (nPrime == 1){
+			break;		
+		}
+	}
 	
 	getchar();
 	return 0;
