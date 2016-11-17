f856b3f83daf91942c6b0a0700d7e4ff336c3682
H-Reinarz <Henning-R@hotmail.de>
2016-11-17 16:25:03 +0100

    17.11 adaptionen

--- experimentJannik/adressbuch_h.c
+++ experimentJannik/adressbuch_h.c
@@ -29,19 +29,49 @@ Bereichsliste* erstelleBereichsliste (enum Bundesland Land){
 };
 
 //Adressen einfügen
-Kunde* NeuerKunde(char name[50+1], int kundenNr){
+int NeuerKunde(char name[50+1], int kundenNr, Bereichsliste* Head){
+	
+	//Neue Node mit Inhalt in den Heap legen
 	Kunde* kunde = (Kunde*) malloc(sizeof(Kunde));
-	//kunde->name = name;
 	strcpy(kunde->name, name);
 	kunde->knr = kundenNr;
 	kunde->next = NULL;
-	return kunde;
-};	
+	
+	
+	//Iteration
+	//Kunde* Iterator, Previous;
+	
+		//Initialisierung
+	Kunde* Iterator = Head->kunde1;
+	Kunde* Previous = Head->kunde1;
+	
+	//Leere Liste	
+	if (Iterator != NULL){
+		//Schleife
+		while (Iterator->next != NULL){
+			//Sortierbedingung
+			if (Iterator->knr < kundenNr){
+				Previous = Iterator;
+				Iterator = Iterator->next;
+			}
+			else if (Iterator->knr == kundenNr){
+				//Kundennummer bereits vergeben
+				return 1;
+			}
+			else {
+				// EINFUEGEN
+				kunde->next = Iterator;
+				Previous->next = kunde;	
+			}
+		}
+	}
+	else {Head->kunde1 = kunde;}
+}	
 
 int main(void) {
 	//Adressbuch anlegen
-	//Adressbuch add = {NULL, NULL, NULL, NULL};
-	printf("Neues Adressbuch:\n");
+	Adressbuch add = {NULL, NULL, NULL, NULL};
+	//printf("Neues Adressbuch:\n");
 	
 	return 0;
-};
+}
